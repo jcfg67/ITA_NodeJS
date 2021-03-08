c6d8642d86cd65fa695362756ea72d96f1fdd4d5
JC <jcfg67@outlook.com>
2021-03-08 11:02:50 +0100

    version1 Sprint3 Node Basics

--- SPRINT3/NODE_BASICS/n3ex1.js
+++ SPRINT3/NODE_BASICS/n3ex1.js
@@ -1,38 +1,38 @@
-// WITH exec AND callback
+// WITH spawn
 
 const os = require('os');
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
 
 const system = os.type();
 const folder = os.homedir();
-
 let readCommand;
 
 switch (system){
     case ('Linux') :
-        readCommand = `ls ${folder}`;
+        readCommand = 'ls';
         break;
     case 'Darwin':
-        readCommand = `ls ${folder}`;
+        readCommand = 'ls';
         break;
     case 'Windows_NT':
-        readCommand = `dir ${folder}`;
+        readCommand = 'dir';
         break;
     default:
         return console.log('OS read directory function not implemented');
 }
 
-exec(readCommand, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    console.error(`stderr: ${stderr}`);         // stderr is attached too with the error object, so this line is not necessary! SEE PS at the bottom
-    return
-  }
-  console.log(stdout);
+const subprocess = spawn(readCommand, [folder], { shell: true });
+
+subprocess.on('error', (err) => {
+  console.error('Failed to start subprocess.');
 });
 
+subprocess.stdout.on('data', (data) => {
+  console.log(data.toString());
+});
 
-
-// PS: In case of an error (including any error resulting in an exit code other than 0), a rejected promise is returned, with the same error object given in the callback, but with two additional properties stdout and stderr.
+subprocess.stderr.on('data', (data) => {
+  console.error(data.toString());
+});
 
 
